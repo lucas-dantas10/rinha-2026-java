f8888835304041609d267e067145b4be5e4e3f6e
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2026-05-13 15:57:01 -0300

    feat: finish vector implementation

--- src/main/java/br/com/rinha/fraud_detection/service/FraudScoreService.java
+++ src/main/java/br/com/rinha/fraud_detection/service/FraudScoreService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class FraudScoreService {
@@ -17,8 +19,23 @@ public class FraudScoreService {
     private final static int MAX_KM = 1000;
     private final static int MAX_TX_COUNT_24H = 20;
     private final static int MAX_MERCHANT_AVG_AMOUNT = 1000;
+    private final static float DEFAULT_VALUE_MCC_RISK = 0.5f;
+    private final static Map<String, Float> mccRisk = new HashMap<>();
 
     private final float[] vector = new float[14];
+
+    public FraudScoreService() {
+        mccRisk.put("5411", 0.15f);
+        mccRisk.put("5812", 0.30f);
+        mccRisk.put("5912", 0.20f);
+        mccRisk.put("5944", 0.45f);
+        mccRisk.put("7801", 0.80f);
+        mccRisk.put("7802", 0.75f);
+        mccRisk.put("7995", 0.85f);
+        mccRisk.put("4511", 0.35f);
+        mccRisk.put("5311", 0.25f);
+        mccRisk.put("5999", 0.50f);
+    }
 
     public void execute(FraudScoreRequest request) {
         TransactionRequest transaction = request.transaction();
@@ -63,17 +80,19 @@ public class FraudScoreService {
         vector[9] = terminal.isOnline() ? 1 : 0;
         vector[10] = terminal.cardPresent() ? 1 : 0;
         vector[11] = knowMerchant ? 0 : 1;
-        vector[12] = 0.5f; // TODO: Verify
+        vector[12] = mccRisk.getOrDefault(merchant.mcc(), DEFAULT_VALUE_MCC_RISK);
         vector[13] = limit(merchant.avgAmount() / MAX_MERCHANT_AVG_AMOUNT);
     }
 
     private float limit(float value) {
         if (value < 0.0) {
             return 0.0f;
-        } else if (value > 1.0) {
-            return 1.0f;
-        } else {
-            return value;
         }
+
+        if (value > 1.0) {
+            return 1.0f;
+        }
+
+        return value;
     }
 }
